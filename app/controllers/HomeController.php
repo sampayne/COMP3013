542840a93ad040bf09c3f4fd67acabfc322c9880
Iustin Sibiescu <sibiescuiustin@gmail.com>
2016-02-16 17:23:51 +0000

    css folder created

---
+++ app/controllers/HomeController.php
@@ -0,0 +1,13 @@
+<?php declare(strict_types=1);
+
+    namespace App\Controller;
+
+    use App\Utility\{Request, Session, View};
+
+    class HomeController extends Controller {
+
+        public function getHomepage(Request $request, Session $session) : string {
+
+            return (new View('home'))->render();
+        }
+    }
