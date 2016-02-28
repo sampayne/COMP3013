da350be0de34c0dd2221c828fa92c1f2cdaf74b1
Ruxandra Panainte <ruxandra-giorgiana.panainte.13@ucl.ac.uk>
2016-02-28 15:41:27 +0000

    More dashboard design of forms and tables.

--- web-app/views/dashboard.php
+++ web-app/views/dashboard.php
@@ -3,22 +3,9 @@
     <h2>Welcome, <?= $user->email ?>!</h2>
     <br />
 
-    <p>id: <?= $user->id ?></p>
+   <!--  <p>id: <?= $user->id ?></p>
     <p>User is a buyer: <?= $user->buyer_role_id ?> </p>
-    <p>User is a seller: <?= $user->seller_role_id ?> </p>
-
-  <!--   <?php if ($condition): ?>
-        <p>Content</p>
-    
-
-    <?php elseif ($other_condition): ?>
-        <p>Other Content</p>
-
-
-    <?php else: ?>
-        <p>Default Content</p>
-
-    <?php endif; ?> -->
+    <p>User is a seller: <?= $user->seller_role_id ?> </p> -->
 
     <?php if($user->isSeller() && $user->isBuyer()): ?>
          <ul class="nav nav-tabs">
@@ -32,14 +19,13 @@
 
     <div id="dashboard-content" class="tab-content">
 
-
         <?php if($user->isSeller()): ?>
 
         <div id="seller-dashboard" class="tab-pane fade in active col-md-12">
 
         <br />
-        <form method="get" action="/auction/create">  
-            <button type="submit">Create Auction</button>
+        <form role="form" method="get" action="/auction/create">  
+            <button type="submit" class="btn btn-default">Create Auction</button>
         </form>
 
             <h3>Live Auctions</h3>
@@ -49,24 +35,32 @@
                 <?php foreach($liveSellerAuctions as $auction){ ?>
     				<tr>
         				<td class="col-md-2"> <img class="col-md-12" src="/images/default.gif"></td>
-                        <td  class="col-md-10">
-                            <?php echo $auction->id; ?> <br />
+                        <td  class="col-md-8">
+<!--                             <?php echo $auction->id; ?> <br /> -->
                             <h4><strong><?php echo $auction->name; ?></strong></h4>
                             <p><em><?php echo $auction->description; ?> </em></p>
                             <p>Starting Price: <?php echo $auction->starting_price; ?> <br />
                             Until: <?php echo $auction->end_date; ?> <br />
-                            Highest Bid: <strong><?php echo $auction->getHighestBid(); ?></strong> <br />
-                            Number of Bids: <?php echo $auction->getBidCount(); ?> <br />
                             Number of Views: <?php echo $auction->getViewCount(); ?> <br />
                             Number of Watches: <?php echo $auction->getWatchCount(); ?> <br /></p> 
-                            <form method="get" action="/auction/<?php echo $auction->id; ?>/edit">  
-                            <button type="submit">Edit Auction</button>
+                            <form role="form" method="get" action="/auction/<?php echo $auction->id; ?>/edit">  
+                            <button type="submit" class="btn btn-default">Edit Auction</button>
                             </form>
                         </td>
-    				</tr>
+    				
+
+                    <td class="col-md-2">
+                            <?php if($auction->getBidCount() != 0): ?>
+                                <h3 class="text-success"><strong> <?php echo $auction->getBidCount(); ?> bids</strong></h3>
+                                <p>Highest Bid: <?php echo $auction->getHighestBid(); ?></p>
+                            <?php else: ?>
+                                <h3 class="text-danger"><strong>No bids</strong></h3>
+                            <?php endif; ?>
+                    </td>
+
+                    </tr>
     			<?php } ?>
 			</table>
-
 
             <h3>Completed Auctions </h3>
 
@@ -74,19 +68,24 @@
                 <?php foreach($completedSellerAuctions as $auction){ ?>
                    <tr>
                         <td class="col-md-2"> <img class="col-md-12" src="/images/default.gif"></td>
-                        <td  class="col-md-10">
-                            <?php echo $auction->id; ?> <br />
+                        <td  class="col-md-8">
+                          <!--   <?php echo $auction->id; ?> <br /> -->
                             <h4><strong><?php echo $auction->name; ?></strong></h4>
                             <p><em><?php echo $auction->description; ?> </em></p>
                             <p>Starting Price: <?php echo $auction->starting_price; ?> <br />
                             Until: <?php echo $auction->end_date; ?> <br />
-                            Highest Bid: <strong><?php echo $auction->getHighestBid(); ?></strong> <br />
-                            Number of Bids: <?php echo $auction->getBidCount(); ?> <br />
                             Number of Views: <?php echo $auction->getViewCount(); ?> <br />
                             Number of Watches: <?php echo $auction->getWatchCount(); ?> <br /></p> 
-                            <form method="get" action="/auction/<?php echo $auction->id; ?>/edit">  
-                            <button type="submit">Edit Auction</button>
-                            </form>
+                        </td>
+
+                        <td class="col-md-3">
+                            <?php if($auction->getBidCount() != 0): ?>
+                                <h3 class="text-success"><strong>Sold</strong></h3>
+                                <p>Sold for: <?php echo $auction->getHighestBid(); ?></p>
+                                Number of Bids: <?php echo $auction->getBidCount(); ?>
+                            <?php else: ?>
+                                <h3 class="text-danger"><strong>Not sold</strong></h3>
+                            <?php endif; ?>
                         </td>
                     </tr>
                 <?php } ?>
@@ -100,9 +99,10 @@
             <table class="pane">
                 <?php foreach($sellerFeedback as $singleFeedback){ ?>
                     <tr>
-                        <td><?php echo $singleFeedback->id; ?>
+                        <td>
+                        <!--  <?php echo $singleFeedback->id; ?> -->
                         <em><?php echo $singleFeedback->content; ?></em>
-                        <p>Create at: <?php echo $singleFeedback->created_at; ?></p></td>
+                        <p class="text-right">Created at: <?php echo $singleFeedback->created_at; ?></p></td>
                     </tr>
                 <?php } ?>
             </table>
@@ -110,14 +110,14 @@
             </div>
             <div class="col-md-4">
 
-            <h3>Stats</h3>
+            <h3>Rating</h3>
                <div class="pane">
 
-                <p>Item as described: <?php echo $sellerRating['mean_item_as_described']; ?></p>
-                <p>Communication: <?php echo $sellerRating['mean_communication']; ?></p>
-                <p>Dispatch Time <?php echo $sellerRating['mean_dispatch_time']; ?></p>
-                <p>Posting: <?php echo $sellerRating['mean_posting']; ?></p>
-                <p>Number of ratings: <?php echo $sellerRating['no_feedback']; ?></p>
+                <p><strong>Item as described:</strong> <?php echo $sellerRating['mean_item_as_described']; ?></p>
+                <p><strong>Communication:</strong> <?php echo $sellerRating['mean_communication']; ?></p>
+                <p><strong>Dispatch Time:</strong> <?php echo $sellerRating['mean_dispatch_time']; ?></p>
+                <p><strong>Posting:</strong> <?php echo $sellerRating['mean_posting']; ?></p>
+                <p><strong>Number of ratings:</strong> <?php echo $sellerRating['no_feedback']; ?></p>
 
                 </div>
             </div>
@@ -152,33 +152,38 @@
                     <tr>
                         <td class="col-md-2"> <img class="col-md-12" src="/images/default.gif"></td>
                         
-                        <td  class="col-md-9">
-                           <?php echo $auction->id; ?> <br />
+                        <td  class="col-md-8">
+                         <!--   <?php echo $auction->id; ?> <br /> -->
                             <h4><strong><?php echo $auction->name; ?></strong></h4>
                             <p><em><?php echo $auction->description; ?> </em></p>
                             <p>End date: <?php echo $auction->end_date; ?><br /></p> 
                             
                         </td>
 
-                        <td class="col-md-1">
+                        <td class="col-md-2">
                             <?php if($auction->getHighestBid() == $auction->getHighestBidForUser($user)): ?>
                                 <h3 class="text-success"><strong>OK</strong></h3>
                                 <p>Highest Bid: <?php echo $auction->getHighestBid(); ?></p>
-                                <p>Bid more:
-                                    <form method="post" action="/auction/<?php echo $auction->id; ?>/bid">  
-                                        <input type="number" name="bid_value" min="<?php echo $auction->getHighestBid() + 1; ?>">
-                                        <button type="submit">Bid</button>
+                                    <form role="form" method="post" action="/auction/<?php echo $auction->id; ?>/bid">  
+                                        <div class="form-group">
+                                        <label>Bid more</label>
+                                        <input class="form-control" type="text" name="bid_value" size="8" min="<?php echo $auction->getHighestBid() + 1; ?>"/>
+                                        </div>
+                                        <button type="submit" class="btn btn-default">Bid</button>
                                     </form>
-                                </p>
+                              
                             <?php else: ?>
                                 <h3 class="text-danger"><strong>Outbid</strong></h3>
-                                <p>Highest Bid: <?php echo $auction->getHighestBid(); ?></p>
-                                <p>Place another bid:
-                                    <form method="post" action="/auction/<?php echo $auction->id; ?>/bid">  
-                                        <input type="number" name="bid_value" min="<?php echo $auction->getHighestBid() + 1; ?>">
-                                        <button type="submit">Bid</button>
+                                <p>Your Bid: <?php echo $auction->getHighestBidForUser($user); ?>
+                                Highest Bid: <?php echo $auction->getHighestBid(); ?></p>
+                          
+                                    <form role="form" method="post" action="/auction/<?php echo $auction->id; ?>/bid">  
+                                        <div class="form-group"><label>Place another bid</label>
+                                        <input class="form-control" type="text" name="bid_value" size="8" min="<?php echo $auction->getHighestBid() + 1; ?>"/>
+                                        </div>
+                                        <button type="submit" class="btn btn-default">Bid</button>
                                     </form>
-                                </p>
+                      
                             <?php endif; ?>
                         </td>
 
@@ -202,14 +207,14 @@
                 <?php foreach($completedBidBuyerAuctions as $auction){ ?>
                     <tr>
                         <td class="col-md-2"> <img class="col-md-12" src="/images/default.gif"></td>
-                        <td  class="col-md-9">
-                           <?php echo $auction->id; ?> <br />
+                        <td  class="col-md-8">
+                         <!--   <?php echo $auction->id; ?> <br /> -->
                             <h4><strong><?php echo $auction->name; ?></strong></h4>
                             <p><em><?php echo $auction->description; ?> </em></p>
                             Ended on: <?php echo $auction->end_date; ?><br /></p> 
                         </td>
 
-                        <td class="col-md-1">
+                        <td class="col-md-2">
                             <?php if($auction->getHighestBid() == $auction->getHighestBidForUser($user)): ?>
                                 <h3 class="text-success"><strong>Won</strong></h3>
                                 <p>Highest Bid: <?php echo $auction->getHighestBid(); ?></p>
@@ -237,21 +242,23 @@
                     
                     <tr>
                         <td class="col-md-2"> <img class="col-md-12" src="/images/default.gif"></td>
-                        <td  class="col-md-9">
-                           <?php echo $auction->id; ?> <br />
+                        <td  class="col-md-8">
+                        <!--    <?php echo $auction->id; ?> <br /> -->
                             <h4><strong><?php echo $auction->name; ?></strong></h4>
                             <p><em><?php echo $auction->description; ?> </em></p>
                             <p>End date: <?php echo $auction->end_date; ?><br /></p> 
                            
                         </td>
 
-                        <td class="col-md-1">
-                            <p>Starting price: <?php echo $auction->starting_price; ?></p> 
-                            <p>Max bid: <?php echo $auction->getHighestBid(); ?></p>
-                            <p>Bid Count: <?php echo $auction->getBidCount(); ?></p>
-                            <form method="post" action="/auction/<?php echo $auction->id; ?>/bid">  
-                            <input type="number" name="bid_value" min="<?php echo $auction->getHighestBid() + 1; ?>">
-                            <button type="submit">Bid</button>
+                        <td class="col-md-2">
+                            <p>Started at: <?php echo $auction->starting_price; ?> 
+                            Highest Bid: <?php echo $auction->getHighestBid(); ?>
+                            Bid Count: <?php echo $auction->getBidCount(); ?></p>
+                            <form role="form" method="post" action="/auction/<?php echo $auction->id; ?>/bid">  
+                            <div class="form-group">
+                            <input class="form-control" type="text" name="bid_value" size="8"min="<?php echo $auction->getHighestBid() + 1; ?>"/>
+                            </div>
+                            <button type="submit" class="btn btn-default">Bid</button>
                             </form>
                         </td>
 
@@ -275,9 +282,9 @@
             <table class="pane">
                 <?php foreach($buyerFeedback as $singleFeedback){ ?>
                     <tr>
-                        <td><?php echo $singleFeedback->id; ?>
+                       <!--  <td><?php echo $singleFeedback->id; ?> -->
                         <em><?php echo $singleFeedback->content; ?></em>
-                        <p>Create at: <?php echo $singleFeedback->created_at; ?></p></td>
+                        <p class="text-right">Created at: <?php echo $singleFeedback->created_at; ?></p></td>
                     </tr>
                 <?php } ?>
             </table>
@@ -297,8 +304,8 @@
 
                 <div class="pane">
 
-                <p>Rating: <?php echo $buyerRating['mean_rating']; ?></p>
-                <p>Number of ratings: <?php echo $buyerRating['no_feedback']; ?></p>
+                <p><strong>Rating:</strong> <?php echo $buyerRating['mean_rating']; ?></p>
+                <p><strong>Number of ratings:</strong> <?php echo $buyerRating['no_feedback']; ?></p>
 
                 </div>
 
@@ -306,9 +313,9 @@
 
             <h3>Stats</h3>
                 <div class="pane">
-                    <p>Auctions won/lost: </p>
-                    <p>Number of bids placed: </p>
-                    <p>Auctions followed: </p>
+                    <p><strong>Auctions won/lost:</strong> </p>
+                    <p><strong>Number of bids placed:</strong> </p>
+                    <p><strong>Auctions followed:</strong> </p>
                 </div>
             </div>
 
