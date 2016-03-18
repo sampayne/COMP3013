d7e8600f11fefe5e4e93fd6f9b62bf1d2b425f0b
sampayne <Sam@Sams-MacBook-Pro.local>
2016-03-18 15:54:14 +0000

    Moves to combined AWS SDK file

--- web-app/views/search.php
+++ web-app/views/search.php
@@ -1,11 +1,10 @@
 <!--$auction_array -->
-<?php if($auctionsFound){?>
-    
-    <div class="container col-md-2 categoryFilter">
+<div class="row">
+    <div class="col-md-3">
         <form role="search" method="get" action="/search">
             <ul class="list-group" >
 
-                <?php foreach($categories as $category){ 
+                <?php foreach($categories as $category){
                     if(in_array($category->name, $selectedCategories)) { ?>
 
                         <li class="list-group-item"><?php echo $category->name; ?><input class="pull-right" type="checkbox" name=<?php echo '"'.($category->name).'"'; ?> value=<?php echo '"'.($category->name).'"'; ?> checked></li>
@@ -22,110 +21,83 @@
             </ul>
         </form>
     </div>
-
-    <div class="row">
-        <?php if($searchTerm != "") {?>
-          
-            <h4>Search results for "<?php echo $searchTerm?>":</h4>
-
-        <?php }?>
-
-        <div class="row">
-        <div class="list-group pane">
-
-            <div class="row list-group-item" style="border-color:transparent; background-color:transparent;">
-
-                <div style="font-size: 20px;" class="pull-left">Sort by:</div>
-                <form class="pull-left" style="margin-left:5px;" method="get" action="/search">
-
-                    <?php if(isset($date) && $date == "0"){ ?>
-                        <input type="hidden" name="date" value="1">
-
-                    <?php }else{ ?>
-                        <input type="hidden" name="date" value="0">
-
-                    <?php }?>
-
-                    <?php foreach($categories as $category){ ?>
-                        <?php if(in_array($category->name, $selectedCategories)) { ?>
-
-                            <input type="hidden" name=<?php echo '"'.($category->name).'"'; ?> value=<?php echo '"'.($category->name).'"'; ?>>
-
-                        <?php }?>
-                    <?php }?> 
-
-                    <input type="hidden" name="search-bar" value=<?php echo '"'.$searchTerm.'"';?>>
-
-                    <button type="submit" class="btn btn-default">Date <?php if(isset($date) && $date == "1"){ ?><span class="glyphicon glyphicon glyphicon-sort-by-attributes-alt" aria-hidden="true"></span><?php }else{?><span class="glyphicon glyphicon glyphicon-sort-by-attributes" aria-hidden="true"></span><?php }?></button>
-                </form>
-
-                <form class="pull-left" style="margin-left:5px;" method="get" action="/search">
-                    
-                    <?php if(isset($price) && $price == "0"){ ?>
-                        <input type="hidden" name="price" value="1">
-
-                    <?php }else{ ?>
-                        <input type="hidden" name="price" value="0">
-
-                    <?php }?>
-
-                    <?php foreach($categories as $category){ ?>
-                        <?php if(in_array($category->name, $selectedCategories)) { ?>
-
-                            <input type="hidden" name=<?php echo '"'.($category->name).'"'; ?> value=<?php echo '"'.($category->name).'"'; ?>>
-
-                        <?php }?>
-                    <?php }?> 
-
-                    <input type="hidden" name="search-bar" value=<?php echo '"'.$searchTerm.'"';?>>
-
-                    <button type="submit" class="btn btn-default">Price <?php if(isset($price) && $price == "1"){ ?><span class="glyphicon glyphicon glyphicon-sort-by-attributes-alt" aria-hidden="true"></span><?php }else{?><span class="glyphicon glyphicon glyphicon-sort-by-attributes" aria-hidden="true"></span><?php }?></button>
-                </form>
-
+    <div class="col-md-9">
+        <div class="panel panel-default">
+            <div class="panel-heading">
+                <h2 class="panel-title">Search results for "<?= $searchTerm ?>"</h2>
             </div>
+            <ul class="list-group">
 
-            <?php foreach ($auctionData as $value) {?>
+                <?php if($auctionsFound):?>
 
-                <a href=<?php echo "/auction/".$value[3] ?> class="list-group-item">
 
-                    <table style="width: 100%;">
-                        <tr>
+                        <li class="list-group-item"> Sort by:
+                        <form class="pull-left" method="get" action="/search">
 
-                            <?php if(strlen($value[5]) != 0 && file_exists($_SERVER['DOCUMENT_ROOT'].$value[5])) {?>
-                                
-                                <td class="col-md-2"> <img class="col-md-12" src=<?php echo($value[5])?>></td>
+                            <?php if(isset($date) && $date == "0"): ?>
+                                <input type="hidden" name="date" value="1">
 
-                            <?php }else{?>
+                            <?php else: ?>
+                                <input type="hidden" name="date" value="0">
 
-                                <td class="col-md-2"> <img class="col-md-12" src="/images/default.gif"></td> <!-- Change path with the path on AWS -->
+                            <?php endif ?>
 
-                            <?php }?> 
+                            <?php foreach($categories as $category): ?>
+                                <?php if(in_array($category->name, $selectedCategories)): ?>
+                                    <input type="hidden" name="<?= $category->name ?>" value="<?= $category->name ?>" >
+                                <?php endif ?>
+                            <?php endforeach ?>
 
-                            <td  class="col-md-8">
+                            <input type="hidden" name="search-bar" value=" <?= $searchTerm ?>">
 
-                                <h4><strong><?php echo $value[0]; ?></strong></h4>
-                                <p><em><?php echo $value[1]; ?> </em></p>
-                                <p>End date: <?php echo $value[2]; ?><br /></p>
+                            <button type="submit" class="btn btn-default">Date<?php if(isset($date) && $date == "1"): ?><span class="glyphicon glyphicon glyphicon-sort-by-attributes-alt" aria-hidden="true"></span><?php else:?><span class="glyphicon glyphicon glyphicon-sort-by-attributes" aria-hidden="true"></span><?php endif ?></button>
+                        </form>
 
-                            </td>
+                        <form class="pull-left" method="get" action="/search">
 
-                            <td style="text-align: center;" class="col-md-2">
-                                <h4><strong><?php echo "£ ".intval($value[4])/100; ?></strong></h4>
-                            </td>
-                        </tr>
-                    </table>
+                            <?php if(isset($price) && $price == "0"): ?>
+                                <input type="hidden" name="price" value="1">
 
-                </a><br>
+                            <?php else: ?>
+                                <input type="hidden" name="price" value="0">
 
-            <?php }?>
+                            <?php endif ?>
 
+                            <?php foreach($categories as $category): ?>
+                                <?php if(in_array($category->name, $selectedCategories)): ?>
+
+                                    <input type="hidden" name=<?php echo '"'.($category->name).'"'; ?> value=<?php echo '"'.($category->name).'"'?>>
+
+                                <?php endif ?>
+                            <?php endforeach ?>
+
+                            <input type="hidden" name="search-bar" value=<?php echo '"'.$searchTerm.'"';?>>
+
+                            <button type="submit" class="btn btn-default">Price <?php if(isset($price) && $price == "1"): ?><span class="glyphicon glyphicon glyphicon-sort-by-attributes-alt" aria-hidden="true"></span><?php else: ?><span class="glyphicon glyphicon glyphicon-sort-by-attributes" aria-hidden="true"></span><?php endif ?></button>
+                        </form>
+
+                        </li>
+
+
+                     <?php foreach ($auction_array as $auction) :?>
+
+                        <li class="list-group-item">
+
+                            <?php include('single_auction.php') ?>
+
+                        </li>
+
+                    <?php endforeach ?>
+
+                <?php else: ?>
+
+                    <li class="list-group-item">
+                        <div class="alert alert-info" role="alert">No results for "<?= $searchTerm ?>"</div>
+                    </li>
+
+                <?php endif ?>
+
+            </ul>
         </div>
-        </div>
-
     </div>
-
-<?php }else{?>
-
-    <h1>Sorry, no auction contains the word "<?php echo $searchTerm?>"</h1>
-
-<?php }?>
+</div>
